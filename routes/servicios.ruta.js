9fa029107e981a4e8f675443a792ea4eb1a5e721
Nelson Segovia <56141280+arse5813@users.noreply.github.com>
2023-08-03 12:35:20 -0400

    Nuevo cambios en la session2

--- routes/servicios.ruta.js
+++ routes/servicios.ruta.js
@@ -9,9 +9,9 @@ const api = express.Router();
 // api.post("/calc/data", [mdAuth.asureAuth], ChatMessageController.sendText)
 // api.get("/calc/data/:calc_id", [mdAuth.asureAuth], ChatMessageController.getAll);
 
-api.post("/serv/data",ServController.registerServicio);
-api.get("/serv/:id",ServController.getServc);
-api.get("/serv/data/:propietario",ServController.getPropietario);
+api.post("/serv/data",[mdAuth.asureAuth],ServController.registerServicio);
+api.get("/serv/:id",[mdAuth.asureAuth],ServController.getServc);
+api.get("/serv/data/:propietario",[mdAuth.asureAuth],ServController.getPropietario);
 
 
 
